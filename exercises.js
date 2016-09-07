7c0eda56524579435d5af6a920527b0aeb29be94
Carlos Rodrigues <carlos.all.mail@gmail.com>
2016-09-07 09:06:10 -0600

    Added exercises for a lesson on creating functions

---
+++ exercises.js
@@ -0,0 +1,45 @@
+var fit_person = {
+  first_name: 'Jeff',
+  middle_name: 'Sue',
+  last_name: 'Wells',
+  steps: 10000
+}
+
+/*
+  Write a function that will return the person's names,
+  concatenated together.
+
+  Call it, and display the results in the console
+*/
+
+// Your code here
+
+
+console.log(  )
+
+/*
+  Write a function that will return the person's middle initial.
+
+  Call it, and display the results in the console
+*/
+
+// Your code here
+
+
+console.log(  )
+
+/*
+  Write a function that will give the encouragement to keep active.
+  If the person has less than 10,000 steps,
+  return an empowering message to keep going.
+
+  If the person has 10,000 steps or more,
+  return a congratulatory message
+
+  Call it, and display the results in the console
+*/
+
+// Your code here
+
+
+console.log(  )
